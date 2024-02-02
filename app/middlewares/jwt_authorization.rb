21f51153e974a3a5ce65a4d29951db1b47baa11e
RajaRaviVarma <rajaravivarma.ar@gmail.com>
2024-02-02 19:44:32 +0530

    Add JWTAuthorization middleware

---
+++ app/middlewares/jwt_authorization.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+# Middleware to authorize using JWTs
+class JWTAuthorization
+  include ResponseHelper
+
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    bearer = env.fetch('HTTP_AUTHORIZATION', '').split[1]&.strip
+    env[:user] = UserSessionToken.get_user(bearer)
+    @app.call(env)
+  rescue JWT::ExpiredSignature
+    error_response(
+      status: 403, error_message: 'The token has expired.'
+    )
+  rescue JWT::DecodeError
+    error_response(
+      status: 401, error_message: 'A token must be passed.'
+    )
+  rescue JWT::InvalidIssuerError
+    error_response(
+      status: 403, error_message: 'The token does not have a valid issuer.'
+    )
+  rescue JWT::InvalidIatError
+    error_response(
+      status: 403, error_message: 'The token does not have a valid "issued at" time.'
+    )
+  end
+
+  private
+
+  def error_response(status:, error_message:)
+    [
+      status,
+      { Header::CONTENT_TYPE => ContentType::APPLICATION_JSON },
+      failure_json_body(token_error(error_message))
+    ]
+  end
+
+  def token_error(message)
+    AppError.new(field: 'token', error_messages: message)
+  end
+end
---
+++ spec/middlewares/jwt_authorization_spec.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+require 'app_helper'
+
+class AuthorizedController < Sinatra::Base
+  use JWTAuthorization
+
+  before do
+    content_type :json
+  end
+
+  get '/test' do
+    status 200
+    { success: true }.to_json
+  end
+
+  post '/test' do
+    status 200
+    { success: true }.to_json
+  end
+end
+
+RSpec.describe JWTAuthorization do
+  def app
+    AuthorizedController
+  end
+
+  def authorization_header(token)
+    { 'HTTP_AUTHORIZATION' => "Bearer #{token}" }
+  end
+
+  let(:email) { 'test@example.com' }
+  let(:password) { 'password' }
+  let(:user) do
+    RegisterNewUser.call(
+      email:, password:, password_verification: password
+    ).value
+  end
+  let(:valid_token) do
+    UserSessionToken.issue(user, expires_in_seconds: 60)
+  end
+  let(:expired_token) do
+    UserSessionToken.issue(user, expires_in_seconds: -60)
+  end
+
+  context 'when a valid token is provided' do
+    it 'calls the app with the user set in the env' do
+      %i[get post].each do |method|
+        send(method, '/test', {}, authorization_header(valid_token))
+
+        response = JSON.parse(last_response.body)
+        expect(last_response.status).to eq(200)
+        expect(response['success']).to be(true)
+      end
+    end
+  end
+
+  context 'when the token has expired' do
+    it 'returns a 403 Forbidden response' do
+      %i[get post].each do |method|
+        send(method, '/test', {}, authorization_header(expired_token))
+
+        response = JSON.parse(last_response.body)
+        expect(last_response.status).to eq(403)
+        expect(response['errors']).to include({ 'token' => ['The token has expired.'] })
+      end
+    end
+  end
+
+  context 'when no token is provided' do
+    it 'returns a 401 Unauthorized response' do
+      %i[get post].each do |method|
+        send(method, '/test', {}, {})
+
+        response = JSON.parse(last_response.body)
+        expect(last_response.status).to eq(401)
+        expect(response['errors']).to include({ 'token' => ['A token must be passed.'] })
+      end
+    end
+  end
+end
