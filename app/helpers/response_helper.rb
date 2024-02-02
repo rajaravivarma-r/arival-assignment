9e45ba62023b519e6030a8cd68f0a8ead4e81733
RajaRaviVarma <rajaravivarma.ar@gmail.com>
2024-02-02 19:30:38 +0530

    Refactor ResponseHelper to be more finer

--- app/helpers/response_helper.rb
+++ app/helpers/response_helper.rb
@@ -2,13 +2,29 @@
 
 # Set of helper functions to send uniform responses
 module ResponseHelper
+  module Header
+    CONTENT_TYPE = 'Content-Type'
+  end
+
+  module ContentType
+    APPLICATION_JSON = 'application/json'
+  end
+
   def success_json(status:, value:)
     status(status)
-    { data: value }.to_json
+    success_json_body(value)
   end
 
   def failure_json(status:, errors:)
     status(status)
+    failure_json_body(errors)
+  end
+
+  def success_json_body(value)
+    { data: value }.to_json
+  end
+
+  def failure_json_body(errors)
     errors = Array(errors)
     error_response = { errors: errors.map(&:to_h) }
     error_response.to_json
